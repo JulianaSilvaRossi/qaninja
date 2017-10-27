f61fe6eca82120c1c55ca97a7a36e4d91dab8965
Juliana Silva Rossi <juliana.rossi@corp.nova>
2017-10-27 09:50:38 -0200

    headless

--- invoices/tests/features/support/env.rb
+++ invoices/tests/features/support/env.rb
@@ -1,5 +1,6 @@
 require 'capybara'
 require 'capybara/cucumber'
+require 'capybara/poltergeist'
 require 'rspec'
 require 'selenium-webdriver'
 require 'site_prism'
@@ -10,6 +11,41 @@ require_relative 'page_object'
 World(PageObjects)
 
 Faker::Config.locale = 'pt-BR'
+
+$browser = ENV['BROWSER']
+
+Capybara.register_driver :selenium do |app|
+
+    if $browser.eql?('chrome')
+        #subir Chrome
+        Capybara::Selenium::Driver.new(
+            app,
+            browser: :chrome,
+            desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+                'chromeOption' => {
+                    'args' => ['windows-size=1280,800']
+                }
+            )
+        )
+
+    elsif $browser.eql?('firefox')
+        #subir Firefox
+        Capybara::Selenium::Driver.new(
+            app,
+            browser: :firefox,
+            marionette: true
+        )
+
+    elsif $browser.eql?('headless')
+        #subir phantonjs + poltergeist
+        Capybara::Poltergeist::Driver.new(
+            app,
+            options = {
+                :js_errors => false
+            }
+        )
+    end
+end
 
 Capybara.configure do |config|
     config.default_driver = :selenium
